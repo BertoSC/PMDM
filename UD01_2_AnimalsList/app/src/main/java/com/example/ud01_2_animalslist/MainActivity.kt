3644dff404d873c99c9d48788e403a25fe5625c2
dam2 - González Chao <A23AlbertoGC@sanclemente.local>
2024-09-18 13:46:23 +0200

    repo

--- UD01_2_AnimalsList/app/src/main/java/com/example/ud01_2_animalslist/MainActivity.kt
+++ UD01_2_AnimalsList/app/src/main/java/com/example/ud01_2_animalslist/MainActivity.kt
@@ -1,16 +1,20 @@
 package com.example.ud01_2_animalslist
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+    override fun onCreate(savedInstanceState: Bundle?) {  //se ejecuta al compilar/crear
+        super.onCreate(savedInstanceState) //llama al método superior
+        enableEdgeToEdge()  //por defecto hace toda la pantalla
         setContentView(R.layout.activity_main)
+        //busca por id, que le añadimos en el paréntesis
+        val btnSend = findViewById<Button>(R.id.btnSend)
+        btnSend.listener
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)

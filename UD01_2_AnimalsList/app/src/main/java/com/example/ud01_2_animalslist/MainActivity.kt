46e47cd3152fdabedaea54b489f3826834b94e0d
A23AlbertoGC <a23albertogc@iessanclemente.net>
2024-09-20 13:25:50 +0200

    prueba

--- UD01_2_AnimalsList/app/src/main/java/com/example/ud01_2_animalslist/MainActivity.kt
+++ UD01_2_AnimalsList/app/src/main/java/com/example/ud01_2_animalslist/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         //busca por id, que le añadimos en el paréntesis
         val btnSend = findViewById<Button>(R.id.btnSend)
-        btnSend.listener
+       // btnSend.setOnClickListener()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)

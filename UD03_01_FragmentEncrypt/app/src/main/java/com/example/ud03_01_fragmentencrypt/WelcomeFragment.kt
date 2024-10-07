59b94a8d17e2136bdab05206ad78583dbb8ce7ff
bertoSC <a23albertogc@iessanclemente.net>
2024-10-07 12:54:21 +0200

    nuevos fragmentos y lógica de botones para transitar entre pantallas

--- UD03_01_FragmentEncrypt/app/src/main/java/com/example/ud03_01_fragmentencrypt/WelcomeFragment.kt
+++ UD03_01_FragmentEncrypt/app/src/main/java/com/example/ud03_01_fragmentencrypt/WelcomeFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
+import kotlin.math.E
 
 class WelcomeFragment : Fragment() {
 
@@ -15,9 +18,17 @@ class WelcomeFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        //infla el diseño desde el fragmento y devuelve una vista inflada
+    ): View? {//infla el diseño desde el fragmento y devuelve una vista inflada
         //return super.onCreateView(inflater, container, savedInstanceState)
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+        //lo metemos en una variable
+        val view = inflater.inflate(R.layout.fragment_welcome, container, false)
+        //recuperamos el boton asociado a la vista
+        val btnStart = view.findViewById<Button>(R.id.btn_start)
+        //añadimos un listener al botón y recuperamos el controlador de navegacion
+                btnStart.setOnClickListener{
+        //con el método navigate pillamos el id de la transicion
+            view.findNavController().navigate(R.id.action_welcomeFragment_to_fragmentMessage)
+        }
+        return view
     }
 }

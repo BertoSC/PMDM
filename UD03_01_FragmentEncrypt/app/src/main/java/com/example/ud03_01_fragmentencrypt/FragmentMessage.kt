5db80f040412fc6e3418aaf2984923d1d161ffb7
bertoSC <a23albertogc@iessanclemente.net>
2024-10-09 13:33:51 +0200

    logica actualizada y plugins nuevos

--- UD03_01_FragmentEncrypt/app/src/main/java/com/example/ud03_01_fragmentencrypt/FragmentMessage.kt
+++ UD03_01_FragmentEncrypt/app/src/main/java/com/example/ud03_01_fragmentencrypt/FragmentMessage.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.navigation.findNavController
 
 class FragmentMessage : Fragment() {
 
@@ -14,8 +17,20 @@ class FragmentMessage : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_message, container, false)
-    }
 
+
+        val view = inflater.inflate(R.layout.fragment_message, container, false)
+
+        val btnNext = view.findViewById<Button>(R.id.btnNext)
+
+        btnNext.setOnClickListener {
+
+            view.findNavController()
+                .navigate(FragmentMessageDirections.actionFragmentMessageToFragmentEncrypt(
+                    (view.findViewById<EditText>(R.id.text_secretMsg).toString())))
+        }
+        return view
+
+    }
 
 }
--- UD03_01_FragmentEncrypt/build.gradle.kts
+++ UD03_01_FragmentEncrypt/build.gradle.kts
@@ -2,5 +2,5 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
-    id("androidx.navigation.safeargs")
+    id("androidx.navigation.safeargs") version "2.5.3" apply false
 }

a628bf796dab19f567977807b744b81a9581151d
bertoSC <a23albertogc@iessanclemente.net>
2024-10-16 13:39:48 +0200

    commit inicial

---
+++ UD04_1_Inbox/app/src/main/java/com/example/ud04_1_inbox/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.ud04_1_inbox
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.appbar.MaterialToolbar
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    // recuperamos la toolbar creada en activitymain y la asociamos a una constante
+     val tool = findViewById<MaterialToolbar>(R.id.toolbar)
+    // asociamos la que recuperamos al soporte de la barra de acción por defecto
+     setSupportActionBar(tool)
+
+    }
+}

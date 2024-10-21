161bdd80114dd9f564d0fb82f94b8994a3f3d223
bertoSC <a23albertogc@iessanclemente.net>
2024-10-21 12:46:51 +0200

    añadidos navs

--- UD04_1_Inbox/app/src/main/java/com/example/ud04_1_inbox/MainActivity.kt
+++ UD04_1_Inbox/app/src/main/java/com/example/ud04_1_inbox/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.ud04_1_inbox
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
@@ -18,10 +25,31 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
     // recuperamos la toolbar creada en activitymain y la asociamos a una constante
      val tool = findViewById<MaterialToolbar>(R.id.toolbar)
-    // asociamos la que recuperamos al soporte de la barra de acción por defecto
+    // le decimos que es la toolbar por defecto
      setSupportActionBar(tool)
 
+     //controlador de navegación
+     val navHostFragment = supportFragmentManager.findFragmentById(R.id.container_fragment) as NavHostFragment
+     val navController = navHostFragment.navController
+
+        tool.setupWithNavController(navController)
+
     }
+
+    //dice cuando una de las opciones es seleccionada, qué vas a hacer
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val navController = findNavController(R.id.container_fragment)
+        NavigationUI.onNavDestinationSelected(item, navController)
+        return super.onOptionsItemSelected(item)
+    }
+
+    //este método se ejecuta cuando se pinta la toolbar por defecto
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_toolbar,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
 }
